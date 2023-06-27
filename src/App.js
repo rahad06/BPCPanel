5550e1822e053ca9f837f0865187234beab017a1
rahad06 <dararah06@gmail.com>
2023-06-27 15:11:30 +0330

    react template

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import Router from './routes';
 // theme
 import ThemeProvider from './theme';
 // components
-// import { StyledChart } from './components/chart';
-// import ScrollToTop from './components/scroll-to-top';
+import { StyledChart } from './components/chart';
+import ScrollToTop from './components/scroll-to-top';
 
 // ----------------------------------------------------------------------
 
 export default function App() {
-    return (
-        <HelmetProvider>
-            <BrowserRouter>
-                <ThemeProvider>
-                    {/*<ScrollToTop />*/}
-                    {/*<StyledChart />*/}
-                    <Router />
-                </ThemeProvider>
-            </BrowserRouter>
-        </HelmetProvider>
-    );
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <ScrollToTop />
+          <StyledChart />
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 }
--- src/components/Main.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Main(props) {
-    return (
-        <div>main</div>
-    );
-}
-
-export default Main;
