ff968098caa4e24d30d4e90ae17b468035f878fe
rahad06 <dararah06@gmail.com>
2023-06-27 15:04:56 +0330

    template done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
-import React from 'react';
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+// routes
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+// components
+// import { StyledChart } from './components/chart';
+// import ScrollToTop from './components/scroll-to-top';
 
-// Import components
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-// import './App.css';
+// ----------------------------------------------------------------------
 
-function App() {
+export default function App() {
     return (
-        <Router>
-            <div className="container">
-                
-           <div className="row justify-content-start">
-               <div className="col-1">
-                       <Navbar/>
-               </div><div className="col">
-            <Routes>
-                <Route exact path="/" element={<Home/>}/>
-                {/*<Route path="/essays" component={Essays}/>*/}
-                {/*<Route path="/research-projects" component={ResearchProjects}/>*/}
-                {/*<Route path="/login" component={Login}/>*/}
-                {/*<Route path="/register" component={Register}/>*/}
-            </Routes>
-           </div></div></div>
-           
-        </Router>
-    )
-        ;
+        <HelmetProvider>
+            <BrowserRouter>
+                <ThemeProvider>
+                    {/*<ScrollToTop />*/}
+                    {/*<StyledChart />*/}
+                    <Router />
+                </ThemeProvider>
+            </BrowserRouter>
+        </HelmetProvider>
+    );
 }
-
-export default App;
